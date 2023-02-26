0c8dcfe2972971bca367a8af99224d16de692c25
moe1011 <77053716+moe1011@users.noreply.github.com>
2023-02-25 19:29:24 -0500

    inti commit

--- calculator/app/src/components/display.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Display({displayValues}){
-    return(
-        <div className="flex bg-emerald-900 w-screen h-20 my-5 px-3 pb-0 items-center 
-        overflow-x-scroll scrollbar scrollbar-thumb-slate-400 scrollbar-track-slate-600
-        hover:scrollbar-thumb-slate-400/80">{displayValues}</div>
-    );
-}
